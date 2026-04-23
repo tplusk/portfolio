67b8185ec793020b900b36c00d5a5a876b4e3191
tplusk <macfelelhun@gmail.com>
2026-04-23 11:12:34 +0100

    add start up point

---
+++ src/Components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+function About() {
+  return;
+}
+
+export default About;
