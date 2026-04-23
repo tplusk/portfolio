9a82e929872f37ebbce0f4f527152cbb2c490693
tplusk <macfelelhun@gmail.com>
2026-04-23 11:21:23 +0100

    add classNames

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 function About() {
-  return;
+  return (
+    <section id="about" className="conatiner text-center">
+      <div className="row">
+        <div className="col-md-8" offset-md-2>
+          <div className="profile-image-container">
+            <h2 className="section-title">
+              <h2 className="section-title">
+                <FontAwesomeIcon
+                  icon={faUser}
+                  className="d-block mx-auto mb-2 text-primary"
+                  size="3x"
+                />
+              </h2>
+            </h2>
+          </div>
+          {/* Needs update */}
+          <p className="section-description mt-4">
+            I am a something... CHANGE IT LATER
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
