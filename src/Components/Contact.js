9bf11f234caced4217d03d68080b7c2c2b8ed1e7
tplusk <macfelelhun@gmail.com>
2026-04-24 14:02:46 +0100

    add imports

---
+++ src/Components/Contact.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Breadcrumb } from "react-bootstrap";
+import "./styles.css";
