89f5f9bd3040798146a00acc86ee1eab4a1c6291
tplusk <macfelelhun@gmail.com>
2026-04-29 11:31:36 +0100

    add breadcrumbsPath

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,3 +4,19 @@ import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Breadcrumb } from "react-bootstrap";
 import "./styles.css";
+
+
+function Contact() {
+    const breadcumbsPath = [
+        {
+            title: 'Home', to: '/'
+        },
+        {
+            title: 'Contact', to: '/contact'
+        }
+    ];
+    
+    return (
+        
+    )
+}
