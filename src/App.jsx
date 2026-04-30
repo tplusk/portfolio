5c972477fd9b0c536e91e32e3ef01e4d3df06e1b
tplusk <macfelelhun@gmail.com>
2026-04-30 15:17:40 +0100

    arrange list of components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Skills from "./Components/Skills";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 
@@ -12,8 +14,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
-          <Contact />
         </Routes>
         <Footer />
       </div>
