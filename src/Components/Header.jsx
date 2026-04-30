50448d80fbb07ac9ac76cb75da054ead52f7954c
tplusk <macfelelhun@gmail.com>
2026-04-30 12:17:12 +0100

    update title

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -17,21 +17,10 @@ const Header = () => {
   return (
     <Navbar bg="dark" expand="lg" className="custom-navbar shadow-sm">
       <Navbar.Brand as={Link} to="/" className="brand-name">
-        <span className="display-4 text-white ho">Geek</span>
+        <span className="display-4 text-white ho">Tamas Kis</span>
       </Navbar.Brand>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
       <div className="collapse navbar-collapse text-white" id="navbarNav">
-        <Nav className="ml-auto ">
+        <Nav className="ml-auto">
           <Nav.Link as={Link} to="/">
             <span>About</span>
             <FontAwesomeIcon
@@ -39,21 +28,19 @@ const Header = () => {
               className="ml-2 text-light ho ho"
             />
           </Nav.Link>
-          <Nav.Link as={Link} to="/projects">
-            <span>Projects</span>
-            <FontAwesomeIcon icon={faCode} className="ml-2 text-light ho" />
-          </Nav.Link>
           <Nav.Link as={Link} to="/resume">
             <span>Resume</span>
             <FontAwesomeIcon icon={faFileAlt} className="ml-2 text-light ho" />
+          </Nav.Link>
+          <Nav.Link as={Link} to="/projects">
+            <span>Projects</span>
+            <FontAwesomeIcon icon={faCode} className="ml-2 text-light ho" />
           </Nav.Link>
           <Nav.Link as={Link} to="/contact">
             <span>Contact</span>
             <FontAwesomeIcon icon={faEnvelope} className="ml-2 text-light ho" />
           </Nav.Link>
-        </Nav>
-        <Nav className="ml-auto">
-          <Nav.Link href="https://github.com/yourusername">
+          <Nav.Link href="https://github.com/tplusk">
             <span className="sr-only">Github</span>
             <FontAwesomeIcon
               icon={faGithub}
@@ -61,7 +48,7 @@ const Header = () => {
               className="text-light ho"
             />
           </Nav.Link>
-          <Nav.Link href="https://www.linkedin.com/in/yourusername">
+          <Nav.Link href="https://www.linkedin.com/in/tamas-kis-1ab34935a/?skipRedirect=true">
             <span className="sr-only">Linkedin</span>
             <FontAwesomeIcon
               icon={faLinkedin}
